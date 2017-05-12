d275d3b45d70414c42a0c2e63a4438205dffb5d4
Pablo López Santori <Pablo Lpez Santori>
2017-05-12 13:52:27 +0100

    Map inits on the page. Added GMaps library

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-var viewModel = {
-
+var model = {
+	initialMap: {
+  	div: '#container-map',
+  	lat: 40.4717956,
+  	lng: -3.8747407
+	},
 }
 
-ko.applyBindings(new viewModel)
+// Not working but I will try to fix it
+
+/*var Map = function(data){
+	this.div = ko.observable(data.div);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+}*/
+
+var viewModel = function () {
+	that = this;
+
+	//that.mapInit = ko.observable(new Map(model.initialMap)); // Try to fix this.
+	that.newMap = ko.observable(new GMaps(model.initialMap));
+};
+
+ko.applyBindings(new viewModel);
