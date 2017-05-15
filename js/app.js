2c504644c808cd3549f5a83f82dc4af6970fb2cb
Pablo López Santori <Pablo Lpez Santori>
2017-05-15 12:35:21 +0100

    Added markers on the map

--- js/app.js
+++ js/app.js
@@ -1,24 +1,61 @@
 var model = {
 	initialMap: {
-  	div: '#container-map',
-  	lat: 40.4717956,
-  	lng: -3.8747407
+  		div: '#container-map',
+  		lat: 40.4717956,
+  		lng: -3.8747407
 	},
-}
+	markers: [
+	{
+		title: "Jardinillos",
+		lat: 40.473665,
+		lng: -3.872893,
+	},
+	{
+		title: "Zoco de Majadahonda",
+		lat: 40.466651,
+		lng: -3.866341,
+	},
+	{
+		title: "Parque Colón",
+		lat: 40.468491,
+		lng: -3.868619,
+	},
+	{
+		title: "Campo de Rugby",
+		lat: 40.475029,
+		lng: -3.881676,
+	}
+	]
+};
 
-// Not working but I will try to fix it
-
-/*var Map = function(data){
-	this.div = ko.observable(data.div);
+var Marker = function (data) {
+	this.title = ko.observable(data.title);
 	this.lat = ko.observable(data.lat);
 	this.lng = ko.observable(data.lng);
-}*/
+}
+
 
 var viewModel = function () {
 	that = this;
 
-	//that.mapInit = ko.observable(new Map(model.initialMap)); // Try to fix this.
-	that.newMap = ko.observable(new GMaps(model.initialMap));
+	this.map = ko.observable(new GMaps(model.initialMap));
+
+	this.allMarkers = ko.observableArray([]);
+
+	model.markers.forEach(function (data) {
+		that.allMarkers.push(new Marker(data))
+	});
+
+	this.allMarkers().forEach(function (data) {
+		that.map().addMarker({
+			lat: data.lat(),
+			lng: data.lng(),
+			title: data.title(),
+			infoWindow: {
+			  content: data.title(),
+			}
+		})
+	});
 };
 
 ko.applyBindings(new viewModel);
