13ef901badff05d09da197350bde59cc741aa316
Pablo López Santori <Pablo Lpez Santori>
2017-05-17 12:43:40 +0100

    added places adress and images

--- js/app.js
+++ js/app.js
@@ -6,24 +6,29 @@ var model = {
 	},
 	markers: [
 	{
-		title: "Jardinillos",
-		lat: 40.473665,
-		lng: -3.872893,
+		title: "La Flaca",
+		lat: 40.466688,
+		lng: -3.866338,
 	},
 	{
-		title: "Zoco de Majadahonda",
-		lat: 40.466651,
-		lng: -3.866341,
+		title: "Cines Zoco",
+		lat: 40.466527,
+		lng: -3.866565,
 	},
 	{
-		title: "Parque Colón",
-		lat: 40.468491,
-		lng: -3.868619,
+		title: "Gastrotasca la Oficina",
+		lat: 40.471198,
+		lng: -3.870349,
 	},
 	{
-		title: "Campo de Rugby",
-		lat: 40.475029,
-		lng: -3.881676,
+		title: "Restaurante la Cuba",
+		lat: 40.475229,
+		lng: -3.877946,
+	},
+	{
+		title: "Casa Pedro",
+		lat: 40.474006,
+		lng: -3.875809,
 	}
 	]
 };
@@ -36,7 +41,7 @@ var Marker = function (data) {
 
 
 var viewModel = function () {
-	that = this;
+	var that = this;
 
 	this.map = ko.observable(new GMaps(model.initialMap));
 
@@ -57,11 +62,39 @@ var viewModel = function () {
 			  content: data.title()
 			},
 			click: function () {
-				// Saves the current object in "this.currentMarker"
+				// Saves the current object in "this.currentMarker".
 				that.currentMarker(data);
 			}
 		})
 	});
+
+	// API for Google images.
+	this.getGoogleImg = ko.computed (function () {
+		var googleKey = "AIzaSyAViOicVJ6HM_KqQdnRORuUyBf832SgvFU";
+		this.imgSrc = ko.observable("https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" +
+			that.currentMarker().title() +
+			", Majadahonda&heading=100&pitch=10&scale=2&key=" + 
+			googleKey);
+		return this.imgSrc();
+
+	})
+
+	this.placeAdress = ko.observable("C/ Plaza de los jardinillos");
+
+	this.getAdress = ko.computed (function (){
+		var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+			that.currentMarker().lat() + "," + that.currentMarker().lng() + "&sensor=true";
+
+		$.getJSON(url, function(data){
+			
+			var object = data.results[0].address_components;
+			var shortAdress = object[1].short_name + ", " + object[0].short_name + ", " + object[6].short_name;
+
+			that.placeAdress(shortAdress)
+			console.log(object)
+			return that.placeAdress();
+		})
+	})
 };
 
 ko.applyBindings(new viewModel);
